ea0a0d1e294ee84b0ca87a4c6f2fbaea8eb688b5
Rafael Artusi <rafael.artusi@gmail.com>
2018-12-01 03:03:41 +0100

    feat: hello-world 0.0.1

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>
+          Hey! Github finder will be available soon! Stay tuned!!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
